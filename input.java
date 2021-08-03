6a30d14754b4d43cc64d6c69ef05c67153c56aa1
huzaifa0786 <huznoone7@gmail.com>
2021-08-03 23:00:30 +0530

    commit

---
+++ input.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class input {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int a=sc.nextInt();
+        System.out.println("Enter the second number");
+        int b=sc.nextInt();
+        int sum=a+b;
+        System.out.println("The sum of the numbers are");
+        System.out.println(sum);
+    
+
+
+
+    }
+    
+}
---
+++ lit.java
@@ -0,0 +1,23 @@
+public class lit {
+    public static void main(String[] args){
+        byte age=34;
+        int age1=34;
+        short age2=45;
+        long ageme=5666666666666L;
+        char ch='A';
+        float f1=5.6f;
+        double d1=4.66d;
+        boolean a=true;
+        System.out.println(age);
+        String st=("MySelf");
+        System.out.println(st);
+
+
+
+
+
+
+
+    }
+    
+}
