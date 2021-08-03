d42f18dbda43d04493d78fbcec9717ee2b0f4842
huzaifa0786 <huznoone7@gmail.com>
2021-08-03 23:28:11 +0530

    commit

---
+++ datatyprchange.java
@@ -0,0 +1,12 @@
+public class datatyprchange {
+    public static void main(String[] args){
+        char grade='B';
+        grade=(char) (grade+8);
+        System.out.println(grade);
+        //decrepyt
+        grade=(char)(grade-8);
+        System.out.println(grade);
+
+    }
+    
+}
